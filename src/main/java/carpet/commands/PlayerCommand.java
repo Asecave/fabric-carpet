97debda23c48d9d68385aee2969256277657a91e
Asecave <ttersch@outlook.de>
2025-10-19 19:12:20 +0200

    fully implemented /alt command

--- src/main/java/carpet/commands/PlayerCommand.java
+++ src/main/java/carpet/commands/PlayerCommand.java
@@ -112,7 +112,55 @@ public class PlayerCommand
                                 ))
                         )
                 );
+        LiteralArgumentBuilder<CommandSourceStack> alt = literal("alt")
+                .requires((player) -> CommandHelper.canUseCommand(player, CarpetSettings.commandAlt))
+                .then(literal("stop").executes(manipulation(EntityPlayerActionPack::stopAll)))
+                .then(makeActionCommand("use", ActionType.USE))
+                .then(makeActionCommand("jump", ActionType.JUMP))
+                .then(makeActionCommand("attack", ActionType.ATTACK))
+                .then(makeActionCommand("drop", ActionType.DROP_ITEM))
+                .then(makeDropCommand("drop", false))
+                .then(makeActionCommand("dropStack", ActionType.DROP_STACK))
+                .then(makeDropCommand("dropStack", true))
+                .then(makeActionCommand("swapHands", ActionType.SWAP_HANDS))
+                .then(literal("hotbar")
+                        .then(argument("slot", IntegerArgumentType.integer(1, 9))
+                                .executes(c -> manipulate(c, ap -> ap.setSlot(IntegerArgumentType.getInteger(c, "slot"))))))
+                .then(literal("kill").executes(PlayerCommand::kill))
+                .then(literal("mount").executes(manipulation(ap -> ap.mount(true)))
+                        .then(literal("anything").executes(manipulation(ap -> ap.mount(false)))))
+                .then(literal("dismount").executes(manipulation(EntityPlayerActionPack::dismount)))
+                .then(literal("sneak").executes(manipulation(ap -> ap.setSneaking(true))))
+                .then(literal("unsneak").executes(manipulation(ap -> ap.setSneaking(false))))
+                .then(literal("sprint").executes(manipulation(ap -> ap.setSprinting(true))))
+                .then(literal("unsprint").executes(manipulation(ap -> ap.setSprinting(false))))
+                .then(literal("look")
+                        .then(literal("north").executes(manipulation(ap -> ap.look(Direction.NORTH))))
+                        .then(literal("south").executes(manipulation(ap -> ap.look(Direction.SOUTH))))
+                        .then(literal("east").executes(manipulation(ap -> ap.look(Direction.EAST))))
+                        .then(literal("west").executes(manipulation(ap -> ap.look(Direction.WEST))))
+                        .then(literal("up").executes(manipulation(ap -> ap.look(Direction.UP))))
+                        .then(literal("down").executes(manipulation(ap -> ap.look(Direction.DOWN))))
+                        .then(literal("at").then(argument("position", Vec3Argument.vec3())
+                                .executes(c -> manipulate(c, ap -> ap.lookAt(Vec3Argument.getVec3(c, "position"))))))
+                        .then(argument("direction", RotationArgument.rotation())
+                                .executes(c -> manipulate(c, ap -> ap.look(RotationArgument.getRotation(c, "direction").getRotation(c.getSource())))))
+                ).then(literal("turn")
+                        .then(literal("left").executes(manipulation(ap -> ap.turn(-90, 0))))
+                        .then(literal("right").executes(manipulation(ap -> ap.turn(90, 0))))
+                        .then(literal("back").executes(manipulation(ap -> ap.turn(180, 0))))
+                        .then(argument("rotation", RotationArgument.rotation())
+                                .executes(c -> manipulate(c, ap -> ap.turn(RotationArgument.getRotation(c, "rotation").getRotation(c.getSource())))))
+                ).then(literal("move").executes(manipulation(EntityPlayerActionPack::stopMovement))
+                        .then(literal("forward").executes(manipulation(ap -> ap.setForward(1))))
+                        .then(literal("backward").executes(manipulation(ap -> ap.setForward(-1))))
+                        .then(literal("left").executes(manipulation(ap -> ap.setStrafing(1))))
+                        .then(literal("right").executes(manipulation(ap -> ap.setStrafing(-1))))
+                ).then(literal("spawn").executes(PlayerCommand::spawn)
+                        .then(literal("atlogout").executes(PlayerCommand::spawn))
+                );
         dispatcher.register(command);
+        dispatcher.register(alt);
     }
 
     private static LiteralArgumentBuilder<CommandSourceStack> makeActionCommand(String actionName, ActionType type)
@@ -144,7 +192,7 @@ public class PlayerCommand
 
     private static ServerPlayer getPlayer(CommandContext<CommandSourceStack> context)
     {
-        String playerName = StringArgumentType.getString(context, "player");
+        String playerName = getPlayerName(context);
         MinecraftServer server = context.getSource().getServer();
         return server.getPlayerList().getPlayerByName(playerName);
     }
@@ -186,7 +234,7 @@ public class PlayerCommand
 
     private static boolean cantSpawn(CommandContext<CommandSourceStack> context)
     {
-        String playerName = StringArgumentType.getString(context, "player");
+        String playerName = getPlayerName(context);
         MinecraftServer server = context.getSource().getServer();
         PlayerList manager = server.getPlayerList();
 
@@ -263,6 +311,17 @@ public class PlayerCommand
             return defaultValue;
         }
     }
+    
+    private static String getPlayerName(CommandContext<CommandSourceStack> context) {
+    	String playerName;
+    	try {
+            playerName = context.getArgument("player", String.class);
+            
+        } catch (IllegalArgumentException e) {
+        	playerName = context.getSource().getDisplayName().getString() + "Two";
+        }
+    	return playerName;
+    }
 
     private static int spawn(CommandContext<CommandSourceStack> context) throws CommandSyntaxException
     {
@@ -301,19 +360,20 @@ public class PlayerCommand
             // Force override flying to false for survival-like players, or they will fly too
             flying = false;
         }
-        String playerName = StringArgumentType.getString(context, "player");
+        String playerName = getPlayerName(context);
         if (playerName.length() > maxNameLength(source.getServer()))
         {
             Messenger.m(source, "rb Player name: " + playerName + " is too long");
             return 0;
         }
+        boolean atLogout = context.getInput().contains("atlogout");
 
         if (!Level.isInSpawnableBounds(BlockPos.containing(pos)))
         {
             Messenger.m(source, "rb Player " + playerName + " cannot be placed outside of the world");
             return 0;
         }
-        boolean success = EntityPlayerMPFake.createFake(playerName, source.getServer(), pos, facing.y, facing.x, dimType, mode, flying);
+        boolean success = EntityPlayerMPFake.createFake(playerName, source.getServer(), pos, facing.y, facing.x, dimType, mode, flying, atLogout);
         if (!success) {
             Messenger.m(source, "rb Player " + playerName + " doesn't exist and cannot spawn in online mode. " +
                     "Turn the server offline or the allowSpawningOfflinePlayers on to spawn non-existing players");
